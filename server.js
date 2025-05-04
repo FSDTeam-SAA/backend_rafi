7255125157d9cbfff75234100739acc796514fd2
saif9795 <adnan.saif9795@gmail.com>
2025-05-05 00:27:38 +0600

    project initialized

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ 
+    status: true, 
+    message: "Welcome to the server!" 
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
