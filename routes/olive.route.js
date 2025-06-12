7f7df6f9d015c8ae441fdd7012d719e0a2d4af7e
rifat-mahmudul <mahmudulhasanrifat094@gmail.com>
2025-06-12 16:21:18 +0600

    csv file uploaded

--- routes/olive.route.js
+++ routes/olive.route.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const { uploadOlive, uploadCSV, getAllOlive, deleteOlive } = require('../controllers/olive.controller');
 const router = express.Router();
 
-const upload = multer({ dest: multer.memoryStorage()  });
+const upload = multer({ storage: multer.memoryStorage() });
 
 // Create from single ticker data
 router.post('/olive',uploadOlive)
