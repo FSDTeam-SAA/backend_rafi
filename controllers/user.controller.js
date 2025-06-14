040e4a42bd8d515cf0c527c6b3c227b77dd3f9c8
Tahsin <gazitahsin323@gmail.com>
2025-06-14 14:35:54 +0600

    `Added singleUser function to user controller and updated routes to include new endpoint`

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,14 +3,14 @@ const User = require("../models/user.model");
 
 exports.updateUser = async (req, res) => {
   try {
-    const { id,email, userName, phoneNumber, address } = req.body
+    const { id, email, userName, phoneNumber, address } = req.body
 
     if (!userName || !email) {
       return res.status(400).send({
-          succuss: false,
-          message: "Please enter all fields",
-          
-        })
+        succuss: false,
+        message: "Please enter all fields",
+
+      })
     }
     let imageLink
     if (req.file) {
@@ -28,7 +28,7 @@ exports.updateUser = async (req, res) => {
 
 
     return res
-    .status(201).send({
+      .status(201).send({
         success: true,
         message: "User updated successfully",
         data: user
@@ -58,5 +58,29 @@ exports.GetAllReffer = async (req, res) => {
       error: error.message
     })
 
+  }
+}
+
+exports.singleUser = async (req, res) => {
+  try {
+    const id = req.params.id
+    const user = await User.findById(id)
+    if (!user) {
+      return res.status(404).json({
+        status: false,
+        message: 'User not found',
+      })
+    }
+    res.status(200).send({
+      status: true,
+      message: 'success',
+      data: user
+    })
+  } catch (error) {
+    res.status(500).send({
+      status: false,
+      message: 'server error',
+      error: error.message
+    })
   }
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { protect, isAdmin } = require('../middlewares/auth.middleware');
 const upload = require('../middlewares/multer.middleware');
-const { updateUser, GetAllReffer } = require('../controllers/user.controller');
+const { updateUser, GetAllReffer, singleUser } = require('../controllers/user.controller');
 
 const router = express.Router();
 
 router.get('/get-refer',GetAllReffer)
 router.post('/update-user',upload.single('imageLink') ,updateUser);
+router.get("/get-user/:id",singleUser)
 
 
 
