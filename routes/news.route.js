da7f8bf00c078632b167c0df6d1fd3e1fc0aff5f
Gzm Fuad Tahsin <73604797+Gzm-Fuad-Tahsin@users.noreply.github.com>
2025-09-29 12:48:19 +0600

    `Reverted and readded "/get-protfolio-news" route in news.route.js`

--- routes/news.route.js
+++ routes/news.route.js
@@ -8,11 +8,11 @@ router.get('/market-news',merketNewsFromAPi)
 router.post('/news/upload', upload.single('file'),uploadCSV)
 router.get('/all-news', getAllNews);    
 router.post('/create-news',protect,isAdmin,upload.single('imageLink'), createNews);
+router.post ("/get-protfolio-news",getMultipleCompanyNews)
 router.get('/:id', getSingleNews);
 router.patch('/:id',protect,isAdmin,upload.single('imageLink'), updateNews);
 router.delete('/:id',protect,isAdmin, deleteNews);
 
-router.post ("/get-protfolio-news",getMultipleCompanyNews)
 
 
 
